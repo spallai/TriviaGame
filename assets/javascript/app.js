a795c8522eae0e0db4c13d2b9e025d9392be86f4
SPallai <sangeeta.sumair@gmail.com>
2018-12-02 03:45:10 -0800

    updated js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,47 @@
-var number = 30;
+var number = 31;
 
 var intervalId;
+
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+run(number);
 
 function run() {
     intervalId = setInterval(decrement, 1000);
 }
 
-run(number);
-
 function decrement() {
     number--;
-    $("#show-number").html("Time left: " + number + " seconds");
+    $("#show-number").html("<h4>Time Remaining: " + number + " seconds </h4>");
     if (number === 0) {
         stop();
-        alert("Time's up!");
+        alert("Correct Answers: " + correctAnswers + " Incorrect answers: " + incorrectAnswers);
     }
 }
 
-function stop () {
+function stop() {
     clearInterval(intervalId);
+    checkAnswers(document.forms['Trivia']);
+}
+
+function checkAnswers(form) {
+    console.log(form.q1answer.value);
+
+    if (form.q1answer.value == 4) {
+        correctAnswers++;
+    }
+    console.log(form.q2answer.value);
+
+    if (form.q2answer.value == 2) {
+        correctAnswers++;
+    }
+
+    console.log(form.q3answer.value);
+
+    if (form.q3answer.value == 4) {
+        correctAnswers++;
+    }
+
+    incorrectAnswers = 3 - correctAnswers;
 }
